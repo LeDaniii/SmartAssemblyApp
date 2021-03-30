79b1f9070730d5661867b3024645ed3dbfcfe2f9
LeDaniii <danielmatzer@gmail.com>
2021-03-30 21:53:19 +0200

    ejs app start

--- ProtoEjs/server.js
+++ ProtoEjs/server.js
@@ -1,11 +1,37 @@
 const mysql = require('mysql');
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 
-console.log(path);
+// console.log(path);
 
 let app = express();
 
 // set view engine
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
+
+// static files
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
+
+//  ---------- Pages Start ----------
+app.get('/', (req, res) => {
+    res.render('pages/index');
+});
+
+app.get('/mainMenu', (req, res) => {
+    res.render('pages/mainMenu');
+});
+
+app.get('/projectLogDirection', (req, res) => {
+    res.render('pages/projectLogDirection');
+});
+
+//  ---------- Pages End ----------
+
+
+app.listen('3000', () => {
+    console.log('server started on port 3000')
+});
