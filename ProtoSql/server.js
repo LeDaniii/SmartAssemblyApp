f138bc9f3e3f7eba5d00d9e968f04b97570af11e
LeDaniii <danielmatzer@gmail.com>
2021-03-30 19:37:54 +0200

    SQL freaking wooooorks! ohhh yeeeeah :)

--- ProtoSql/server.js
+++ ProtoSql/server.js
@@ -1,15 +1,22 @@
 const mysql = require('mysql');
 const express = require('express');
+const path = require('path');
+
+console.log(path);
 
 let app = express();
 
 // set view engine
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended:true }));
 
 // index pages
 // app.get('/', (req, res) => {
 //     res.render('pages/index');
 // })
+
+
+
 
 //  about pages
 app.get('/about', function(req, res) {
@@ -64,88 +71,107 @@ db.connect((err) => {
 
 
 
-// Create db
-app.get('/createdb', (req, res) => {
-    let sql = 'CREATE DATABASE nodemysql';
-    db.query(sql, (err, result) => {
+
+app.post('/submit_form', (req, res) => {
+    let title = req.body.title;
+    let posts = req.body.posts;
+    let sql = `INSERT INTO posts (title, body) VALUES ('${title}','${posts}')`;
+    let query = db.query(sql, (err, result) => {
         if (err) throw err;
-        console.log(result)
-        res.send('database created');
-    });
+        console.log(result);
+        res.redirect('/about')
+    })
+    // console.log(sql);
+    // console.log(title);
+    // console.log(posts);
+    // console.log(req.body.email + 'this is the email');
+    // console.log(req.body.message + ':this is the message');
 });
+
+
+
+// Create db
+// app.get('/createdb', (req, res) => {
+//     let sql = 'CREATE DATABASE nodemysql';
+//     db.query(sql, (err, result) => {
+//         if (err) throw err;
+//         console.log(result)
+//         res.send('database created');
+//     });
+// });
 
 // create tables
-app.get('/createposttable', (req, res) => {
-    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255), body VARCHAR(255))';
-    db.query(sql, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send('Posts table created....');
-    });
-});
+// app.get('/createposttable', (req, res) => {
+//     let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255), body VARCHAR(255))';
+//     db.query(sql, (err, result) => {
+//         if (err) throw err;
+//         console.log(result);
+//         res.send('Posts table created....');
+//     });
+// });
 
 // insert post 1
-app.get('/addpost1', (req, res) => {
-    let post = { title: 'Post One', body: 'This is post number one' };
-    let sql = 'INSERT INTO posts SET ?';
-    let query = db.query = db.query(sql, post, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send('Post 1 added....');
-    })
-});
+// app.get('/addpost1', (req, res) => {
+//     let post = { title: 'Post One', body: 'This is post number one' };
+//     let sql = 'INSERT INTO posts SET ?';
+//     let query = db.query = db.query(sql, post, (err, result) => {
+//         if (err) throw err;
+//         console.log(result);
+//         res.send('Post 1 added....');
+//     })
+// });
 
 // insert post 2
-app.get('/addpost2', (req, res) => {
-    let post = { title: 'Post One', body: 'This is post number one' };
-    let sql = 'INSERT INTO posts SET ?';
-    let query = db.query = db.query(sql, post, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send('Post 2 added....');
-    })
-});
+// app.get('/addpost2', (req, res) => {
+//     let post = { title: 'Post One', body: 'This is post number one' };
+//     let sql = 'INSERT INTO posts SET ?';
+//     let query = db.query = db.query(sql, post, (err, result) => {
+//         if (err) throw err;
+//         console.log(result);
+//         res.send('Post 2 added....');
+//     })
+// });
 
 // select posts
-app.get('/getposts', (req, res) => {
-    let sql = 'SELECT * FROM posts';
-    let query = db.query(sql, (err, results) => {
-        if (err) throw err;
-        console.log(results);
-        res.send('post fetched...');
-    })
-});
+// app.get('/getposts', (req, res) => {
+//     let sql = 'SELECT * FROM posts';
+//     let query = db.query(sql, (err, results) => {
+//         if (err) throw err;
+//         console.log(results);
+//         res.send('post fetched...');
+//     })
+// });
 
 // select single post
-app.get('/getposts/:id', (req, res) => {
-    let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`;
-    let query = db.query(sql, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send('single post fetched...');
-    })
-});
+// app.get('/getposts/:id', (req, res) => {
+//     let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`;
+//     let query = db.query(sql, (err, result) => {
+//         if (err) throw err;
+//         console.log(result);
+//         res.send('single post fetched...');
+//     })
+// });
 
 // Update Posts
-app.get('/updateposts/:id', (req, res) => {
-    let newTitle = 'Updated Title';
-    let sql = `UPDATE posts SET title = '${newTitle}' WHERE id = ${req.params.id}`;
-    let query = db.query(sql, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send('post updated...');
-    })
-});
+// app.get('/updateposts/:id', (req, res) => {
+//     let newTitle = 'Updated Title';
+//     let sql = `UPDATE posts SET title = '${newTitle}' WHERE id = ${req.params.id}`;
+//     let query = db.query(sql, (err, result) => {
+//         if (err) throw err;
+//         console.log(result);
+//         res.send('post updated...');
+//     })
+// });
 
 // Delete Post
-app.get('/deleteposts/:id', (req, res) => {
-    let sql = `Delete FROM posts WHERE id = ${req.params.id}`;
-    let query = db.query(sql, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send('post deleted...');
-    })
-});
+// app.get('/deleteposts/:id', (req, res) => {
+//     let sql = `Delete FROM posts WHERE id = ${req.params.id}`;
+//     let query = db.query(sql, (err, result) => {
+//         if (err) throw err;
+//         console.log(result);
+//         res.send('post deleted...');
+//     })
+// });
 
 // let mysqlConnection = mysql.createConnection({
 //     host: 'localhost',
